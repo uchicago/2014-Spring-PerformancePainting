4f00e1caec1179a0a454087883698d1b5a93455f
T. Andrew Binkowski <abinkowski@uchicago.edu>
2014-05-04 21:46:13 -0500

    Initial Commit

---
+++ Spring-2014-PerformancePainting/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Spring-2014-PerformancePainting
+//
+//  Created by T. Andrew Binkowski on 5/4/14.
+//  Copyright (c) 2014 The University of Chicago, Department of Computer Science. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Spring-2014-PerformancePainting/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Spring-2014-PerformancePainting
+//
+//  Created by T. Andrew Binkowski on 5/4/14.
+//  Copyright (c) 2014 The University of Chicago, Department of Computer Science. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
